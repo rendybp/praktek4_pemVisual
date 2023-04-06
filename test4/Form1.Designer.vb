221e31335b91f75fe6180a357e8c21a190a402cf
rendybp <rendibuanap@gmail.com>
2023-04-06 19:33:20 +0700

    Statement Majemuk If Apotik Kondang Waras

--- test4/Form1.Designer.vb
+++ test4/Form1.Designer.vb
@@ -22,17 +22,113 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.jumlahInput = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnUlangi = New System.Windows.Forms.Button()
+        Me.btnProses = New System.Windows.Forms.Button()
+        Me.beliInput = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(345, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(323, 36)
+        Me.Label1.TabIndex = 48
+        Me.Label1.Text = "Apotik Kondang Waras"
+        '
+        'jumlahInput
+        '
+        Me.jumlahInput.Location = New System.Drawing.Point(431, 205)
+        Me.jumlahInput.Name = "jumlahInput"
+        Me.jumlahInput.Size = New System.Drawing.Size(357, 26)
+        Me.jumlahInput.TabIndex = 47
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(225, 205)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 20)
+        Me.Label3.TabIndex = 45
+        Me.Label3.Text = "Jumlah"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(225, 143)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 20)
+        Me.Label2.TabIndex = 44
+        Me.Label2.Text = "Nama Barang"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(543, 288)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 55)
+        Me.btnExit.TabIndex = 51
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnUlangi
+        '
+        Me.btnUlangi.Location = New System.Drawing.Point(462, 288)
+        Me.btnUlangi.Name = "btnUlangi"
+        Me.btnUlangi.Size = New System.Drawing.Size(75, 55)
+        Me.btnUlangi.TabIndex = 50
+        Me.btnUlangi.Text = "Ulangi"
+        Me.btnUlangi.UseVisualStyleBackColor = True
+        '
+        'btnProses
+        '
+        Me.btnProses.Location = New System.Drawing.Point(381, 288)
+        Me.btnProses.Name = "btnProses"
+        Me.btnProses.Size = New System.Drawing.Size(75, 55)
+        Me.btnProses.TabIndex = 49
+        Me.btnProses.Text = "Proses"
+        Me.btnProses.UseVisualStyleBackColor = True
+        '
+        'beliInput
+        '
+        Me.beliInput.FormattingEnabled = True
+        Me.beliInput.Items.AddRange(New Object() {"Paracetamol", "Paramex"})
+        Me.beliInput.Location = New System.Drawing.Point(431, 134)
+        Me.beliInput.Name = "beliInput"
+        Me.beliInput.Size = New System.Drawing.Size(357, 28)
+        Me.beliInput.TabIndex = 52
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(959, 674)
+        Me.ClientSize = New System.Drawing.Size(1047, 695)
+        Me.Controls.Add(Me.beliInput)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnUlangi)
+        Me.Controls.Add(Me.btnProses)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.jumlahInput)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents jumlahInput As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnUlangi As System.Windows.Forms.Button
+    Friend WithEvents btnProses As System.Windows.Forms.Button
+    Friend WithEvents beliInput As System.Windows.Forms.ComboBox
 
 End Class
--- test4/Form1.vb
+++ test4/Form1.vb
@@ -1,3 +1,48 @@
 ﻿Public Class Form1
 
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        jumlahInput.Text = ""
+        jumlahInput.ReadOnly = True
+        btnProses.Enabled = False
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles beliInput.SelectedIndexChanged
+        jumlahInput.ReadOnly = False
+        btnProses.Enabled = True
+        If beliInput.Text.Trim = "" Then
+            jumlahInput.Text = ""
+            jumlahInput.ReadOnly = True
+            btnProses.Enabled = False
+        End If
+        jumlahInput.Focus()
+    End Sub
+
+
+    Private Sub btnProses_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnProses.Click
+        Dim brg As String
+        Dim jml As Integer
+
+        If IsNumeric(jumlahInput.Text) Then
+            brg = beliInput.Text
+            jml = CInt(jumlahInput.Text)
+            If brg = "Paracetamol" And jml > 3 Then
+                MessageBox.Show("Anda akan mendapat bonus 1 hand sanitizer")
+            ElseIf brg = "Paramex" Then
+                MessageBox.Show("Anda akan mendapat diskon 10%")
+            Else
+                MessageBox.Show("Terima kasih atas kunjungannya")
+            End If
+        End If
+    End Sub
+
+    Private Sub btnUlangi_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUlangi.Click
+        beliInput.Text = ""
+        jumlahInput.Text = ""
+        jumlahInput.ReadOnly = True
+        btnProses.Enabled = False
+    End Sub
 End Class
